7244a85daca8a9a56abe4bc9bd062fbe15ddb852
Shivam8568 <69636233+Shivam8568@users.noreply.github.com>
2020-12-31 14:51:05 +0700

    Add files via upload

---
+++ C68/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import TransactionScreen from "./screens/BookTransactionScreen";
+import SearchScreen from "./screens/SearchScreen";
+import Facebook from "./screens/Facebook";
+import Instagram from "./screens/Instagram";
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Facebook: { screen: Facebook },
+  Instagram: { screen: Instagram }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
